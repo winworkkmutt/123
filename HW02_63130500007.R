2e64b8d117b0b12a2d7f1e92daaa98edc3ccc52b
ThigerColor <72536652+winworkkmutt@users.noreply.github.com>
2021-10-22 14:21:22 +0700

    af,ZW,f"L,egv"Xr,g';er

---
+++ HW02_63130500007.R
@@ -0,0 +1,13 @@
+########################### week 2 ##########################################
+install.packages("MASS")
+library("MASS")
+View(cats)
+View(survey)
+
+summary(factor(survey$Sex))
+summary(factor(survey$Smoke))
+-- หาความถี่ของการสูบบุหรี่ กับ ไม่สูบบุหรี่ี่มากที่สุด
+
+
+
+
